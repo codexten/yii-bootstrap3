216a5802381a4eaef93175387495800638953bf1
Jomon Johnson <jomon.entero@gmail.com>
2019-03-02 11:01:55 +0530

    init

---
+++ config/bootstrap3.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 2/3/19
+ * Time: 10:58 AM
+ */
+
+return [
+    'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => [
+                        '@codexten/yii/bootstrap3/views',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
